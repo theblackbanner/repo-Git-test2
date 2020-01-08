23487d0f8f0cb749c42e799bb56e1387d769dd82
theblackbanner <maz.dodo25@gmail.com>
2020-01-08 23:49:54 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 <body>
 <h1>Hello World!</h1>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi quia fuga eaque quibusdam laboriosam corrupti, cupiditate architecto necessitatibus minima excepturi quisquam qui, temporibus laborum, incidunt itaque recusandae. Molestias, reprehenderit reiciendis?</p>
-    
+<ul>
+    <li>Satu</li>
+    <li>Dua</li>
+</ul>
 </body>
 </html>
